6aa66a93cbdc204ad1352c2d077282560d88c271
tddag <danhtam0509@gmail.com>
2023-04-22 22:22:11 -0400

    added images, finished register and login pages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Add from "../img/addAvatar.png"
 
 const Register = () => {
   return (
@@ -11,7 +12,10 @@ const Register = () => {
                 <input type="email" placeholder="email"/>
                 <input type="password" placeholder="password"/>
                 <input style={{ display: "none"}} type="file" id="file"/>
-                <label htmlFor="file"></label>
+                <label htmlFor="file">
+                  <img src={Add} alt=""/>
+                  <span>Add an avatar</span>
+                </label>
                 <button>Sign up</button>
             </form>
             <p>You do have an account? Login</p>
