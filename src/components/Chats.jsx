cbae79fef0c7e4288456497506f021959ec65261
tddag <danhtam0509@gmail.com>
2023-04-24 16:36:50 -0400

    fetch chat conversations for current user

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+
+import { AuthContext } from "../context/AuthContext"
 
 const Chats = () => {
+
+  const [chats, setChats] = useState([])
+
+  const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data())
+      });
+  
+      return () => {
+        unsub()
+      }
+    }
+
+    currentUser.uid && getChats()
+
+  }, [currentUser.uid])
+
+  console.log(Object.entries(chats))
+
   return (
     <div className="chats">
-      <div className="userChat">
-        <img src="https://images.pexels.com/photos/10384486/pexels-photo-10384486.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
+      { Object.entries(chats)?.map((chat) => (
+        <div className="userChat" key={chat[0]}>
+        <img src={chat[1].userInfo.photoURL}/>
         <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
+          <span>{chat[1].userInfo.displayName}</span>
+          <p>{chat[1].lastMessage?.text}</p>
         </div>
-      </div>      
+        </div>    
+      ))}
+  
       
     </div>
   )
