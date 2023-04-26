f844cacd60cb2ca5156b62817fb4710eba30cfee
tddag <danhtam0509@gmail.com>
2023-04-26 18:38:15 -0400

    scroll to latest message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { AuthContext } from '../context/AuthContext'
 import { ChatContext } from '../context/ChatContext'
 
@@ -7,8 +7,16 @@ const Message = ({ message }) => {
   const { currentUser } = useContext(AuthContext)
   const { data} = useContext(ChatContext)
 
+  const ref = useRef()
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({behavior: "smooth"})
+
+  }, [message])
+  
+
   return (
-    <div className={`message ${message.senderId === currentUser.uid && "owner"}`}>
+    <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
       <div className="messageInfo"> 
         <img 
           src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} 
